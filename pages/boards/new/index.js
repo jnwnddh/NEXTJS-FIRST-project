00f4273b7f5bf88fda130b1d4a4d47fe285da353
jnwnddh <98944538+jnwnddh@users.noreply.github.com>
2022-12-27 03:49:34 +0900

    FEAT 수정기능추가내일보완

--- pages/boards/new/index.js
+++ pages/boards/new/index.js
@@ -1,16 +1,8 @@
 import BoardWrite from "../../../src/components/units/board/write/BoardWrite.container";
-
-  
   //글쓰기페이지
-  
-
-
   export default function BoardsNew() {
-    
-
-
     return (
-     <BoardWrite/>
+     <BoardWrite isEdit={false}/>
     );
   }
   

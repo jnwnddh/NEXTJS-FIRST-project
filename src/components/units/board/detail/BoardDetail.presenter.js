9ff170fd2fece986439cae36b3c7edd77c7b8ed0
jnwnddh <98944538+jnwnddh@users.noreply.github.com>
2022-12-26 13:56:55 +0900

    데브최초커밋

--- src/components/units/board/detail/BoardDetail.presenter.js
+++ src/components/units/board/detail/BoardDetail.presenter.js
@@ -2,6 +2,7 @@ export default function BoardDetailUI(props){
 
     return (
       <>
+        <div>상세페이지</div>
         <div>{props.data?.fetchBoard?.writer}</div>
         <div>{props.data?.fetchBoard?.createdAt}</div>
         <div>{props.data?.fetchBoard?.title}</div>

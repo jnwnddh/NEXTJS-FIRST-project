e6bb8b4edc6ee1e49985ec30f8427d0d111c56ed
jnwnddh <98944538+jnwnddh@users.noreply.github.com>
2022-12-31 22:05:20 +0900

    상세페이지 좋아요싫어요

--- src/components/units/board/detail/BoardDetail.type.ts
+++ src/components/units/board/detail/BoardDetail.type.ts
@@ -4,4 +4,6 @@ export interface IBoardDetailUIProps {
   data?: Pick<IQuery, "fetchBoard">;
   //void=아무것도리턴하지않고잇다
   onClickDelete: (event: MouseEvent<HTMLButtonElement>) => void;
+  onClickLike: () => void;
+  onClickDislike: () => void;
 }

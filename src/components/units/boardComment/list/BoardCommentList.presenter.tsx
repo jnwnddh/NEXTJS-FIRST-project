cfbbc1cc27340b85b3c52f576fe4e12f406697b8
jnwnddh <98944538+jnwnddh@users.noreply.github.com>
2022-12-31 22:25:32 +0900

    FEAT댓글별점추가

--- src/components/units/boardComment/list/BoardCommentList.presenter.tsx
+++ src/components/units/boardComment/list/BoardCommentList.presenter.tsx
@@ -1,19 +1,31 @@
 import { getMyDate } from "../../../commons/utils/utils";
+import { PasswordInput, PasswordModal, Star } from "./BoardCommentList.styles";
 import { IBoardCommentListUIProps } from "./BoardCommentList.type";
 
 export default function BoardCommentListUI(props: IBoardCommentListUIProps) {
   console.log(props?.data?.fetchBoardComments);
   return (
     <div>
+      {props.isOpenDeleteModal && (
+        <PasswordModal visible={true} onOk={props.onClickDelete}>
+          <div>비번입력</div>
+          <PasswordInput
+            type="password"
+            onChange={props.onChangeDeletePassword}
+          />
+        </PasswordModal>
+      )}
+
       {props.data?.fetchBoardComments?.map((el, _id) => {
         return (
           <div key={el._id}>
             <div>작성자:{el.writer}</div>
             <div>내용:{el.contents}</div>
+            <Star value={el.rating} disabled />
+            <div>{getMyDate(el?.createdAt)}</div>
             <button id={el._id} onClick={props.onClickDelete}>
               삭제
             </button>
-            <div>{getMyDate(el?.createdAt)}</div>
           </div>
         );
       })}
--- src/components/units/boardComment/list/BoardCommentList.type.ts
+++ src/components/units/boardComment/list/BoardCommentList.type.ts
@@ -1,7 +1,10 @@
-import { MouseEvent } from "react";
+import { ChangeEvent, MouseEvent } from "react";
 import { IQuery } from "../../../../commons/types/generated/types";
-//댓글가져올때타입명시
+
 export interface IBoardCommentListUIProps {
   data?: Pick<IQuery, "fetchBoardComments">;
-  onClickDelete: (event: MouseEvent<HTMLButtonElement>) => void;
+  isOpenDeleteModal: boolean;
+  onClickDelete: (event: MouseEvent<HTMLElement>) => void;
+  onClickOpenDeleteModal: (event: MouseEvent<HTMLImageElement>) => void;
+  onChangeDeletePassword: (event: ChangeEvent<HTMLInputElement>) => void;
 }
